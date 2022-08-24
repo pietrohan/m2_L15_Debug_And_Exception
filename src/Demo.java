d3ea698db24c8f6c2f306a6770e9076019408a48
HanPham <petphamhandc@gmail.com>
2022-08-24 16:39:46 +0700

    debug and ngoai le

---
+++ src/Demo.java
@@ -0,0 +1,53 @@
+public class Demo {
+    public static void main(String[] args) {
+//       *  Các kịch bản phổ biến nơi ngoại lệ có thể xảy ra
+//
+//        1. Arithmetic Exception
+//
+//        Nếu chúng ta chia bất kỳ số nào cho số 0, xảy ra ngoại lệ ArithmeticException.
+//
+//        Ví dụ:
+
+//        int a = 10 / 1; // ArithmeticException
+//        System.out.println(a);
+
+//        2.Null Pointer Exception
+//
+//         Nếu bất kỳ biến nào có giá trị null, thực hiện bất kỳ hoạt động nào bởi biến đó sẽ xảy ra ngoại lệ NullPointerException.
+//
+//                Ví dụ:
+////
+//        String obj = "null";
+//
+//        System.out.println(obj .length()); // NullPointerException
+
+//        3. Number Format Exception
+//
+//        Một biến String có giá trị là các ký tự, chuyển đổi biến này thành số sẽ xảy ra NumberFormatException.
+//
+//                String str = "1";
+//
+//        int num = Integer.parseInt(str); // NumberFormatException
+
+//        4. Array Index Out Of Bounds Exception
+//
+//        Nếu bạn chèn bất kỳ giá trị nào vào index sai, sẽ xảy ra ngoại lệ ArrayIndexOutOfBoundsException.
+
+//                Ví dụ:
+//
+//        int arr[] = new int[5];
+//
+//        arr[4] = 50; // ArrayIndexOutOfBoundsException
+
+//        5. Class Cast Exception
+//
+//        Nếu không thể chuyển kiểu object này sang kiểu object khác, sẽ xảy ra ngoại lệ ClassCastException.
+
+//                Ví dụ:
+
+//        Object dog = new Dog();
+//
+//        Rectangle rect = (Rectangle) dog;
+    }
+}
+
